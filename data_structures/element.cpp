a23dd41bbca5d6b2ac15359280d673428a426fed
Dubourg Theo <plop@plop.fake>
2013-03-11 11:12:35 +0100

    with xml tags

--- data_structures/element.cpp
+++ data_structures/element.cpp
@@ -19,9 +19,10 @@ Element* Element::getParent() {
 
 string Element::toXML() {
 	stringstream str;
-	str << this->name << endl;
+	str << "<" << this->name << ">" << endl;// open the XML tag
 	for(nodeList::iterator it = this->children->begin(); it != this->children->end(); ++it) {
 		str << (*it)->toXML();
 	}
+	str << "</" << this->name << ">" << endl;// close the XML tag
 	return str.str();
 }
