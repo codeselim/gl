499c1badb971f11103f10017c84c4251602bc91b
tdubourg <plop@plop.fake>
2013-03-19 19:54:02 +0100

    Added a main file that grabs the arguments from the CLI + added a debug display

--- data_structures/main.cpp
+++ data_structures/main.cpp
@@ -1,5 +1,33 @@
 #include "element.h"
-
-int main() {
+#include <iostream>
+using namespace std;
+#define DBG
+int main(int argc, char** argv) {
+	// Note : We do not handle errors of values not being here arrays of things like that as the CLI interface
+	// is manage by the "cli" file and we are thus garantueed there will always be a first argument 
+	// (excluded the binary's name) that is the XML file name and the 
+	// second and third arguments are going to be DTD and XSL
+	char* xmlfile = argv[1]; 
+#ifdef DBG
+		cout << "XML file: " << xmlfile << endl;
+#endif
+	char*dtdfile, *xslfile;
+	bool is_dtd = false, is_xsl = false;
+	if(argc > 2) {
+		is_dtd = true;
+		dtdfile = argv[2];
+#ifdef DBG
+		cout << "DTD: " << dtdfile << endl;
+#endif
+	}
+	if(argc > 3) {
+		is_xsl = true;
+		xslfile = argv[3];
+#ifdef DBG
+		cout << "DTD: " << xslfile << endl;
+#endif
+	}
+	// Put main test logic here
+	
 	return 0;
 }
