65a80aaa285544a89423f216067bb11dd958d70e
atoumia <amri.toumia@insa-lyon.fr>
2013-03-20 08:53:54 +0100

    test compilation

--- Validation/Validate.h
+++ Validation/Validate.h
@@ -1,3 +1,11 @@
+#ifndef __VALIDATE_H_
+
+#define __VALIDATE_H_
+
+#include <string>
+#include "../data_structures/document.h"
+#include "../data_structures/dtd.h"
+#include "../data_structures/element.h"
 
 class Validate {
 
@@ -7,7 +15,10 @@ protected:
 
 public:
 
-	Validate(Document *xmlFile, Dtdt *dtdFile);
+	Validate(Document *xmlFile, Dtd *dtdFile);
 	bool isValid();
+	bool isElementValid(Element *elt);
 
 };
+
+#endif
