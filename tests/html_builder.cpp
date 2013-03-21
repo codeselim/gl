8b1578eb4a91a074cadc093e08707ac664aab19e
tdubourg <plop@plop.fake>
2013-03-22 00:10:57 +0100

    added a test

--- tests/html_builder.cpp
+++ tests/html_builder.cpp
@@ -84,6 +84,75 @@ bool html_building(){
 }
 
 
+bool html_building2(){
+
+        //<xml>
+        //<a><b><c></c></b></a>
+        //</xml>
+        nodeList lroot, la, lb;
+        Node* b = new TextNode(string("Blorg"));
+        lroot.push_back(b);
+        ElementName enroot = ElementName(string("blorg"), string("xml"));
+        Element* xmlroot = Element::createElement(&enroot, NULL, &lroot);
+
+        ElementName ena = ElementName(string("blorg"), string("a"));
+        Element* _a = Element::createElement(&ena, NULL, &la);
+
+        lroot.push_back(_a);
+
+        ElementName enb = ElementName(string("blorg"), string("b"));
+        Element* _b = Element::createElement(&enb, NULL, &lb);
+
+        la.push_back(_b);
+
+        ElementName enc = ElementName(string("blorg"), string("c"));
+        Element* _c = Element::createElement(&enc, NULL, NULL);
+
+        lb.push_back(_c);
+
+        //<xsl:stylesheet> 
+        //<xsl:template match="xml">
+        //<html>
+        //<body>
+        //<xsl:apply-templates/>
+        //</body>
+        //</html>
+        //</xsl:template>
+        //</xsl:stylesheet>
+        nodeList n;
+        nodeList n1;
+        nodeList n2;
+        nodeList n3;
+        nodeList n4;
+        ElementName dt = ElementName(string("xsl"), string("stylesheet"));
+        ElementName dt1 = ElementName(string("xsl"), string("template"));
+        ElementName dt2 = ElementName(string("blorg"), string("html"));
+        ElementName dt3 = ElementName(string("blorg"), string("body"));
+        ElementName dt4 = ElementName(string("xsl"), string("apply-templates"));
+        attributesMap am;
+        am["match"] = "xml";
+        Node* f = Element::createElement(&dt4, NULL, &n4);
+        n3.push_back(f);
+        Node* e = Element::createElement(&dt3, NULL, &n3);
+        n2.push_back(e);
+        Node* d = Element::createElement(&dt2, NULL, &n2);
+        n1.push_back(d);
+        XSLElement* c1 = (XSLElement*) Element::createElement(&dt1, &am, &n1);
+        n.push_back(c1);
+        XSLElement* root = (XSLElement*) Element::createElement(&dt, NULL, &n);
+
+        HTMLBuilder htmlb(root, xmlroot);
+        string result = htmlb.html();
+
+        string expected = "<html>\n<body>\nBlorg<a>\n<b>\n<c>\n</c>\n</b>\n</a>\n</body>\n</html>";
+        if (expected != result)
+        {
+                fail("html_building", "Result did not match expected.\nExpected: \n-----------------\n" << expected << "\n-----------------\nResult: \n-----------------\n" << result << "\n-----------------" << std::endl)
+        }
+        return true;
+}
+
+
 
 bool html_building_without_a_apply_templates() {
 
@@ -145,12 +214,14 @@ int main(int argc, char** argv) {
 	test_func tests[] = {
 		htmlb_instanciation,
                 html_building,
+                html_building2,
                 html_building_without_a_apply_templates
 	};
 	// insert here your new test names :
 	const char* tests_names[] = {
 		"htmlb_instanciation",
                 "html_building",
+                "html_building2",
                 "html_building_without_a_apply_templates"
 	};
 	// increment this number each time you add a new test
